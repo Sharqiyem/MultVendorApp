4025a13e8243c920695d537ceb54b8a0a5482ad4
Abdulsalam <sharqiyem@yandex.ru>
2020-04-20 07:35:05 +0800

    Add chat

---
+++ src/core/chatHelper.js
@@ -0,0 +1,94 @@
+import firebase from '../config/firebase.config';
+import React from 'react';
+export default class ChatHelper {
+  //Create chat
+  static create() {
+    const data = {
+      uid: firebase.auth().currentUser.uid,
+      count: 0,
+      createdAt: Date.now(),
+      messages: [],
+    };
+    firebase.firestore().collection('chats').add(data);
+  }
+
+  static sendMessage(chatId = 'chat1', data) {
+    console.log(data);
+    // const data = {
+    //   uid: firebase.auth().currentUser.uid,
+    //   text,
+    //   createdAt: Date.now(),
+    // };
+    const db = firebase.firestore();
+    const ref = db.collection('chats').doc(chatId);
+
+    return ref.update({
+      messages: firebase.firestore.FieldValue.arrayUnion(data[0]),
+    });
+  }
+
+  static useGetMessages = (chatId = 'chat1') => {
+    const [data, setData] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
+    React.useEffect(() => {
+      //isCancel to prevent "Can't perform a React state update on an unmounted component"
+      let isCancelled = false;
+
+      let unsubscribe;
+      try {
+        // console.log('useGetDataByCollection useEffect');
+        unsubscribe = firebase
+          .firestore()
+          .collection('chats')
+          .doc(chatId)
+          .onSnapshot((snapShot) => {
+            // console.log('useGetDataByCollection onSnapshot');
+            let messages = snapShot.data()?.messages;
+            // .orderBy('createdAt', 'asc');
+
+            if (messages) {
+              // use inverted in gift chat instead of sort
+
+              // messages = messages?.sort((a, b) => {
+              //   a = a.createdAt.toDate();
+              //   b = b.createdAt.toDate();
+              //   return b - a;
+              // });
+              const newData = messages.map((message) => ({
+                // id: doc._id,
+                // ...message,
+                _id: message._id,
+                createdAt: message.createdAt.toDate(),
+                text: message.text,
+                user: {
+                  _id: message.user._id,
+                  name: message.user.name,
+                  // avatar: getAvatar(message.data().user),
+                },
+              }));
+              if (!isCancelled) {
+                setData(newData);
+                setIsLoading(false);
+              }
+            } else {
+              setData([]);
+              setIsLoading(false);
+            }
+          });
+      } catch (err) {
+        //TODO : try to catch error on use logout
+        alert(`useGetDataByCollection chat err` + err);
+      }
+      return () => {
+        console.log(`unsubscribe useGetDataByCollection chat`);
+        isCancelled = true;
+        if (unsubscribe) {
+          unsubscribe();
+          console.log(`unsubscribe useGetDataByCollection chat`);
+        }
+      };
+    }, []);
+
+    return [data, isLoading];
+  };
+}
---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  ActivityIndicator,
+} from 'react-native';
+import { GiftedChat, MessageText } from 'react-native-gifted-chat';
+import firebase from '../config/firebase.config';
+
+import ChatHelper from '../core/chatHelper';
+import Colors from '../constants/Colors';
+import { Feather } from '@expo/vector-icons';
+import getStyle from '../constants/styles';
+
+const ChatScreen = ({ navigation, route }) => {
+  const { orderId: chatId } = route.params;
+  console.log('chatId', chatId);
+  const [data, isLoading] = ChatHelper.useGetMessages(chatId);
+
+  const [messages, setMessages] = React.useState([]);
+
+  React.useEffect(() => {
+    // const msgs = [
+    //   {
+    //     _id: '1',
+    //     text: 'Hello developer',
+    //     createdAt: new Date(),
+    //     user: {
+    //       _id: '2',
+    //       name: 'Abdul',
+    //       // avatar: 'https://placeimg.com/140/140/any',
+    //     },
+    //   },
+    //   {
+    //     _id: '2',
+    //     text: 'Hello user',
+    //     createdAt: new Date(Date.UTC(2020, 5, 11, 17, 20, 0)),
+    //     user: {
+    //       _id: '1',
+    //       name: 'React',
+    //       // avatar: 'https://placeimg.com/140/140/1',
+    //     },
+    //   },
+    //   {
+    //     _id: '3',
+    //     text: 'This is a system message',
+    //     createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+    //     system: true,
+    //     // Any additional custom parameters are passed through
+    //   },
+    // ];
+    console.log('useGetMessages data', data);
+    setMessages(data);
+  }, [data]);
+
+  const user = {
+    _id: firebase.auth().currentUser?.uid,
+    name: firebase.auth().currentUser?.displayName,
+  };
+  const send = (newMessages) => {
+    newMessages[0].createdAt = new Date();
+    // const msgs = [...newMessages, ...messages];
+    // setMessages(msgs);
+    console.log('newMessages', newMessages);
+    ChatHelper.sendMessage(chatId, newMessages);
+  };
+
+  return (
+    <View
+      style={[{ flex: 1, paddingTop: 38, backgroundColor: Colors.primary }]}
+    >
+      <View style={[{ flexDirection: 'row' }]}>
+        <Feather
+          name='arrow-left'
+          size={25}
+          style={{ marginHorizontal: 10, alignSelf: 'center' }}
+          color={Colors.white}
+          onPress={() => {
+            navigation.pop();
+          }}
+        />
+
+        <Text
+          style={{
+            padding: 10,
+            flex: 1,
+            textAlign: 'center',
+            fontSize: 16,
+            color: Colors.white,
+          }}
+        >
+          Chat {chatId}
+        </Text>
+      </View>
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        {!isLoading ? (
+          <View style={{ flex: 1, backgroundColor: '#fff' }}>
+            <GiftedChat
+              messages={messages}
+              onSend={(messages) => {
+                send(messages);
+              }}
+              user={user}
+              inverted={false}
+              renderMessageText={({ currentMessage, ...args }) => {
+                return (
+                  <MessageText
+                    currentMessage={currentMessage}
+                    customTextStyle={{ fontSize: 14, lineHeight: 40 }} // or more it's depend whats font you use
+                    {...args}
+                  />
+                );
+              }}
+            />
+            {/* <KeyboardAvoidingView
+            behavior={Platform.OS === 'android' ? 'padding' : null}
+            keyboardVerticalOffset={20}
+          /> */}
+          </View>
+        ) : (
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          >
+            <ActivityIndicator
+              style={{
+                alignSelf: 'center',
+                width: Layout.window.width,
+              }}
+              size={'large'}
+              color={Colors.primary}
+            />
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default ChatScreen;
